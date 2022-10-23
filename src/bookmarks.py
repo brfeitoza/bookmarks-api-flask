338e9d25e14d3c50c9945e731d97147f327b6728
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2022-10-23 08:51:36 -0300

    post and get bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,8 +1,58 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+import validators
 
-bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.database import Bookmark, db
+
+bookmarks = Blueprint('bookmarks', __name__, url_prefix='/api/v1/bookmarks')
 
 
-@bookmarks.get("/")
-def get_all():
-    return {"bookmarks": []}
+@bookmarks.route('/', methods=['POST', 'GET'])
+@jwt_required()
+def handle_bookmarks():
+    current_user = get_jwt_identity()
+
+    if request.method == 'POST':
+        body = request.get_json().get('body', '')
+        url = request.get_json().get('url', '')
+
+        if not validators.url(url):
+            return jsonify({
+                'error': 'Enter a valid url'
+            }), HTTP_400_BAD_REQUEST
+
+        if Bookmark.query.filter_by(url=url).first():
+            return jsonify({
+                'error': 'URL already exists'
+            }), HTTP_409_CONFLICT
+
+        bookmark = Bookmark(url=url, body=body, user_id=current_user)
+
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visits': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at,
+        }), HTTP_201_CREATED
+
+    else:
+        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+
+        data = [{
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visits': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at,
+        } for bookmark in bookmarks]
+
+        return jsonify({'data': data}), HTTP_200_OK
