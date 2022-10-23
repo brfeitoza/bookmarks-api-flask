11f81add928e2314d96439e8360e441fe7057dec
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2022-10-23 09:32:34 -0300

    update and delete bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 import validators
 
-from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 from src.database import Bookmark, db
 
 bookmarks = Blueprint('bookmarks', __name__, url_prefix='/api/v1/bookmarks')
@@ -89,6 +89,61 @@ def get_bookmark(id):
         'url': bookmark.url,
         'short_url': bookmark.short_url,
         'visits': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at,
+    }), HTTP_200_OK
+
+
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            'message': 'Bookmark not found'
+        }), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return jsonify({}), HTTP_204_NO_CONTENT
+
+
+@bookmarks.put('/<int:id>')
+@bookmarks.patch('/<int:id>')
+@jwt_required()
+def edit_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            'message': 'Bookmark not found'
+        }), HTTP_404_NOT_FOUND
+
+    body = request.get_json().get('body', '')
+    url = request.get_json().get('url', '')
+
+    if not validators.url(url):
+        return jsonify({
+            'error': 'Enter a valid url'
+        }), HTTP_400_BAD_REQUEST
+
+    bookmark.url = url
+    bookmark.body = body
+
+    db.session.commit()
+
+    return jsonify({
+        'id': bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visit': bookmark.visits,
         'body': bookmark.body,
         'created_at': bookmark.created_at,
         'updated_at': bookmark.updated_at,
