d77bdcc1f50dd0445765f943aa4a66cc9c539887
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2022-10-23 10:02:35 -0300

    error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect
+from flask import Flask, jsonify, redirect
 from flask_jwt_extended import JWTManager
 import os
 
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from src.database import db, Bookmark
 
 
@@ -38,5 +39,17 @@ def create_app(test_config=None):
             db.session.commit()
 
             return redirect(bookmark.url)
+
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({
+            'error': 'Not found'
+        }), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({
+            'error': 'Something went wrong, we are working on it'
+        }), HTTP_500_INTERNAL_SERVER_ERROR
 
     return app
